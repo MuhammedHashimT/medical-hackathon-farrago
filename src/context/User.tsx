0b5dc0a6db7810df256b3d868cd7ed4ec8ee7930
Janishnehyan03 <nehyanjanish@gmail.com>
2024-02-02 19:11:39 +0530

    updated

--- src/context/User.tsx
+++ src/context/User.tsx
@@ -2,57 +2,61 @@
 
 import Axios from "@/utils/Axios";
 import {
-    createContext,
-    useContext,
-    Dispatch,
-    SetStateAction,
-    useState,
-    useEffect,
+  createContext,
+  useContext,
+  Dispatch,
+  SetStateAction,
+  useState,
+  useEffect,
 } from "react";
+import Cookies from "js-cookie";
 
 type DataType = {
-    FirstName: string
-    LastName: string
-    email: string
-    username: string
-    id: number
+  FirstName: string;
+  LastName: string;
+  email: string;
+  username: string;
+  id: number;
 };
 
 interface ContextProps {
-    user: DataType | null;
-    setUser: Dispatch<SetStateAction<DataType | null>>;
+  user: DataType | null;
+  setUser: Dispatch<SetStateAction<DataType | null>>;
 }
 
 const UserContext = createContext<ContextProps>({
-    user: {} as DataType | null,
-    setUser: (): DataType | null => {
-        return {} as DataType | null;
-    },
+  user: {} as DataType | null,
+  setUser: (): DataType | null => {
+    return {} as DataType | null;
+  },
 });
 
 export const UserContextProvider = ({ children }: any) => {
-    const [user, setUser] = useState<DataType | null>({} as DataType || null);
+  const [user, setUser] = useState<DataType | null>(({} as DataType) || null);
 
-    useEffect(() => {
-        console.log(localStorage.getItem('token'));
-        
-       Axios.get('/users/checkLoggedIn',{headers:{
-        Authorization: `${localStorage.getItem('token')}`
-       }}).then((data) => {
+  useEffect(() => {
+    const cookie = Cookies.get("token");
+
+    Axios.get("/users/checkLoggedIn", {
+      headers: {
+        Authorization: `${cookie}`,
+      },
+    })
+      .then((data) => {
         console.log(data);
-        if(data?.data?.username){
-            setUser(data.data);
+        if (data?.data?.username) {
+          setUser(data.data);
         }
-       }).catch((err) => {
-              console.log(err.response);
-         }
-         );
-    }, []);
-    return (
-        <UserContext.Provider value={{ user , setUser }}>
-            {children}
-        </UserContext.Provider>
-    );
+      })
+      .catch((err) => {
+        console.log(err.response);
+      });
+  }, []);
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
 };
 
 export const useUserContext = () => useContext(UserContext);
--- src/utils/Axios.ts
+++ src/utils/Axios.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const Axios = axios.create({
-  baseURL: "http://192.168.0.54:5000/api/" , //"https://medical-hackathon-server.vercel.app/api/",
+  // baseURL: "http://localhost:5000/api/" ,
+  baseURL: "https://medical-hackathon-server.vercel.app/api/",
   headers: {
-    "Content-Type": "application/json", 
+    "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Headers": "*",
     "Access-Control-Allow-Methods": "*",
