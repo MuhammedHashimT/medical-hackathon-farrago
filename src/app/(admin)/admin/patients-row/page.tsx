896df8a4710421adbd9aea066255eedb6b411a87
MuhammedHashimT <hashimt4567@gmail.com>
2024-02-03 10:18:49 +0530

    Add ag-grid dependencies and update global CSS styles

---
+++ src/app/(admin)/admin/patients-row/page.tsx
@@ -0,0 +1,12 @@
+import PatientsRow from '@/components/PatientsRow'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <PatientsRow />
+    </div>
+  )
+}
+
+export default page
