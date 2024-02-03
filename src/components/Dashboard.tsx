966c09a59b11931b489f9f6623f8135e37030601
new-user6663 <nothinguses3@proton.me>
2024-02-03 14:40:35 +0530

    dashboad changes

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -25,68 +25,74 @@ function Dashboard() {
   }, []);
 
   return (
-    <div className="w-full">
-      <div className="flex justify-between items-center">
-        <img className="w-64 h-16 object-contain" src="/logo/logo.png" />
-        <h1 className="text-center font-bold text-4xl">Dashboard</h1>
-        <button className="text-white max-h-8 px-2 py-1 bg-blue-700">
-          Hello User
-        </button>
-      </div>
-
+    <div className="bg-blurblue flex flex-col justify-center items-center h-screen bg-contain p-20">
+      <h2 className="text-center text-[40px] font-extrabold">
+        <span className="text-primary">My</span> Profile
+      </h2>
       {loading && <h1>Loading...</h1>}
-      <h2 className="text-xl font-semibold mb-4">Health Information</h2>
+      <h2 className="text-xl font-semibold mb-4">
+        Health Details and Personal Information{" "}
+      </h2>
       {data && (
-        <div className="grid grid-cols-3 gap-y-5 mx-4">
-          <div>
-            <h3 className="text-lg font-semibold mb-2">Personal Information</h3>
-            <p>
-              Name: {data.userId.firstName} {data.userId.lastName}
-            </p>
-            <p>Username: {data.userId.username}</p>
-            <p>Email: {data.email}</p>
-            <p>Phone Number: {data.phoneNumber}</p>
-            <p>
-              Address: {data.address}, {data.state}, {data.country}
-            </p>
-            <p>Date of Birth: {new Date(data.dob).toLocaleDateString()}</p>
-            <p>Gender: {data.gender}</p>
+        <div className="flex">
+          <div className="grid grid-cols-2 gap-y-5 mx-4 bg-white p-10 rounded-xl border border-gray-200 ">
+            <div>
+              <h3 className="text-lg font-semibold mb-2">
+                Personal Information
+              </h3>
+              <p>
+                Name: {data.userId.firstName} {data.userId.lastName}
+              </p>
+              <p>Username: {data.userId.username}</p>
+              <p>Email: {data.email}</p>
+              <p>Phone Number: {data.phoneNumber}</p>
+              <p>
+                Address: {data.address}, {data.state}, {data.country}
+              </p>
+              <p>Date of Birth: {new Date(data.dob).toLocaleDateString()}</p>
+              <p>Gender: {data.gender}</p>
+            </div>
+            <div>
+              <h3 className="text-lg font-semibold mb-2">Health Metrics</h3>
+              <p>Blood Group: {data.bloodGroup}</p>
+              <p>Height: {data.height} cm</p>
+              <p>Weight: {data.weight} kg</p>
+            </div>
+            <div>
+              <h3 className="text-lg font-semibold mb-2">Health Conditions</h3>
+              <p>Allergies: {data.allergies.join(", ")}</p>
+              <p>Medications: {data.medications.join(", ")}</p>
+              <p>Surgeries: {data.surgeries.join(", ")}</p>
+            </div>
+            <div>
+              <h3 className="text-lg font-semibold mb-2">
+                Health Metrics Details
+              </h3>
+              <p>
+                Blood Pressure: {data.bloodPressure.systolic}/
+                {data.bloodPressure.diastolic} mmHg
+              </p>
+              <p>
+                Cholesterol Level: Total: {data.cholesterolLevel.total}, HDL:{" "}
+                {data.cholesterolLevel.hdl}, LDL: {data.cholesterolLevel.ldl}
+              </p>
+              <p>
+                Blood Sugar Level: Fasting: {data.bloodSugarLevel.fasting},
+                Post-Prandial: {data.bloodSugarLevel.postPrandial}
+              </p>
+            </div>
           </div>
-          <div>
-            <h3 className="text-lg font-semibold mb-2">Health Metrics</h3>
-            <p>Blood Group: {data.bloodGroup}</p>
-            <p>Height: {data.height} cm</p>
-            <p>Weight: {data.weight} kg</p>
+          <div className="bg-white border flex flex-col justify-center border-gray-200 rounded-xl p-10">
+            <img src="/img/robot.gif" className="w-56"/>
+            <Link
+              href={"/chat-bot"}
+              className=" flex-col items-center justify-center"
+            >
+              <h1 className="text-center m-auto text-white px-2 py-1 rounded-lg bg-primary">
+                Interact with AI Assistant
+              </h1>
+            </Link>
           </div>
-          <div>
-            <h3 className="text-lg font-semibold mb-2">Health Conditions</h3>
-            <p>Allergies: {data.allergies.join(", ")}</p>
-            <p>Medications: {data.medications.join(", ")}</p>
-            <p>Surgeries: {data.surgeries.join(", ")}</p>
-          </div>
-          <div>
-            <h3 className="text-lg font-semibold mb-2">
-              Health Metrics Details
-            </h3>
-            <p>
-              Blood Pressure: {data.bloodPressure.systolic}/
-              {data.bloodPressure.diastolic} mmHg
-            </p>
-            <p>
-              Cholesterol Level: Total: {data.cholesterolLevel.total}, HDL:{" "}
-              {data.cholesterolLevel.hdl}, LDL: {data.cholesterolLevel.ldl}
-            </p>
-            <p>
-              Blood Sugar Level: Fasting: {data.bloodSugarLevel.fasting},
-              Post-Prandial: {data.bloodSugarLevel.postPrandial}
-            </p>
-          </div>
-          <Link
-            href={"/chat-bot"}
-            className="bg-blue-600 text-white flex-col items-center justify-center"
-          >
-            <h1 className="text-center m-auto">Interact with our assistant</h1>
-          </Link>
         </div>
       )}
     </div>
